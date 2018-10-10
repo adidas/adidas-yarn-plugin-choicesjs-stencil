1156a4084f94840feed6809bda9142c8f0c04361
Jaime Pastor <jaime.pastor@adidas-group.com>
2018-10-10 17:29:08 +0200

    Add example

---
+++ example/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-container">
+    <headerbar/>
+    <main class="main">
+      <nuxt/>
+    </main>
+    <foot/>
+  </div>
+</template>
+
+<script>
+import headerbar from '~/components/headerbar/default';
+import foot from '~/components/foot/default';
+
+export default {
+  components: {
+    headerbar,
+    foot
+  }
+};
+</script>
+
+<style lang="less">
+  @import '~@adidas/yarn-design-system/src/less/variables/main';
+
+  .main {
+    @media screen and (max-width: @screen-xs-max) {
+      margin-bottom: 10rem;
+    }
+  }
+</style>
